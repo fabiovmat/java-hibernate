ceff7b6fa745f0325376380c82bb60448fb4c0b9
fabio <fabio.meihua08@gmail.com>
2020-05-20 02:46:39 -0300

    adicionado trazendo lista de usuarios na tela atraves do ManagedBean

---
+++ src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -0,0 +1,47 @@
+package managedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
+@ManagedBean(name = "usuarioPessoaManagedBean")
+@ViewScoped
+public class UsuarioPessoaManagedBean {
+
+	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
+	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+	private List<UsuarioPessoa> list = new ArrayList<UsuarioPessoa>();
+	
+	
+	public UsuarioPessoa getUsuarioPessoa() {
+		return usuarioPessoa; //get e set somente do usuario pessoa nao precisa DaoGeneric
+	}
+	public void setUsuarioPessoa(UsuarioPessoa usuarioPessoa) {
+		this.usuarioPessoa = usuarioPessoa;
+	}
+	
+	public String salvar() {
+		
+	daoGeneric.salvar(usuarioPessoa);
+	
+		return "";
+}
+	
+	public String novo(){
+		
+		usuarioPessoa = new UsuarioPessoa();
+		return "";
+	}
+	
+	
+	public List<UsuarioPessoa> getList() {
+		list = daoGeneric.listar(UsuarioPessoa.class);
+		return list;
+	}
+	
+}
