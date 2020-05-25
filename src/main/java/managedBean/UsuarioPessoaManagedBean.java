cb27ffe8dc8fde38f6e7126748162dab65e8e53c
fabio <fabio.meihua08@gmail.com>
2020-05-25 11:26:28 -0300

    Implementacao do Webservice CEP com integração JSF

--- src/main/java/managedBean/UsuarioPessoaManagedBean.java
+++ src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -1,5 +1,10 @@
 package managedBean;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +13,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import com.google.gson.Gson;
 
 import dao.DaoUsuario;
 import model.UsuarioPessoa;
@@ -17,73 +25,107 @@ import model.UsuarioPessoa;
 public class UsuarioPessoaManagedBean {
 
 	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
-	
+
 	private List<UsuarioPessoa> list = new ArrayList<UsuarioPessoa>();
 	private DaoUsuario<UsuarioPessoa> daoGeneric = new DaoUsuario<UsuarioPessoa>();
-	
-	
+
 	@PostConstruct
 	public void init() {
 		list = daoGeneric.listar(UsuarioPessoa.class);
 	}
-	
-	
-	
-	public UsuarioPessoa getUsuarioPessoa() {
-		return usuarioPessoa; //get e set somente do usuario pessoa nao precisa DaoGeneric
+
+	public void pesquisaCep(AjaxBehaviorEvent event) {
+		try {
+
+			// System.out.println("Cep digitado + " + usuarioPessoa.getCep());
+			URL url = new URL("https://viacep.com.br/ws/" + usuarioPessoa.getCep() + "/json/"); // pega o cep que foi
+																								// digitado
+			URLConnection connection = url.openConnection(); // abre uma conexao com a url
+			InputStream is = connection.getInputStream(); // abre uma conexao com fluxo de dados
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8")); // le o fluxo de dados vindo
+																						// como retorno com objeto de
+																						// inpoutstream e utiliza
+																						// codificaçao UTF para tratar
+																						// acentuacação
+
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+
+			while ((cep = br.readLine()) != null) { // le o retorno e enquanto for difrente de nulo
+				jsonCep.append(cep);// adiciona
+
+			}
+
+			UsuarioPessoa userCepPessoa = new Gson().fromJson(jsonCep.toString(), UsuarioPessoa.class);			
+			
+			usuarioPessoa.setCep(userCepPessoa.getCep());
+			usuarioPessoa.setLogradouro(userCepPessoa.getLogradouro());
+			usuarioPessoa.setComplemento(userCepPessoa.getComplemento());
+			usuarioPessoa.setBairro(userCepPessoa.getBairro());
+			usuarioPessoa.setLocalidade(userCepPessoa.getLocalidade());
+			usuarioPessoa.setUf(userCepPessoa.getUf());
+			usuarioPessoa.setUnidade(userCepPessoa.getUnidade());
+			usuarioPessoa.setIbge(userCepPessoa.getIbge());
+			usuarioPessoa.setGia(userCepPessoa.getGia());
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
+
+	public UsuarioPessoa getUsuarioPessoa() {
+		return usuarioPessoa; // get e set somente do usuario pessoa nao precisa DaoGeneric
+	}
+
 	public void setUsuarioPessoa(UsuarioPessoa usuarioPessoa) {
 		this.usuarioPessoa = usuarioPessoa;
 	}
-	
+
 	public String salvar() {
-		
-	daoGeneric.salvar(usuarioPessoa);
-	list.add(usuarioPessoa);
-	FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação: ", "Salvo com sucesso!"));
+
+		daoGeneric.salvar(usuarioPessoa);
+		list.add(usuarioPessoa);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Salvo com sucesso!"));
 		return "";
-}
-	
-	public String novo(){
-		
+	}
+
+	public String novo() {
+
 		usuarioPessoa = new UsuarioPessoa();
 		return "";
 	}
-	
-	
+
 	public List<UsuarioPessoa> getList() {
-		
+
 		return list;
 	}
-	
+
 	public String remover() {
-		
+
 		try {
 			daoGeneric.removerUsuario(usuarioPessoa);
 			list.remove(usuarioPessoa);
 			usuarioPessoa = new UsuarioPessoa();
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação","Deletado com sucesso!"));
-			
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação", "Deletado com sucesso!"));
+
 		} catch (Exception e) {
 			if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ","Existem telefones para o usuário!"));
-			}else {
-				
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Informação: ", "Existem telefones para o usuário!"));
+			} else {
+
 				e.printStackTrace();
-				
+
 			}
-		
+
 		}
 		return "";
-		
-	}
-	
-	
-	
-		
-		
-	}
-	
-	
-	
 
+	}
+
+}
